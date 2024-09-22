81b46b0b296297c6e87533addd1eb1114a95046d
Md Abdullah Al Mahadi Apurbo <abdullahalmahadiapurbo@gmail.com>
2024-09-22 12:04:48 +0600

    Delete Frontend directory

--- Frontend/src/hooks/useRoleSelection.ts
+++
@@ -1,8 +0,0 @@
-export const useRoleSelection = (form: any) => {
-    const handleRoleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-      form.setFieldValue('role', event.target.value);
-    };
-  
-    return { handleRoleChange };
-  };
-  
